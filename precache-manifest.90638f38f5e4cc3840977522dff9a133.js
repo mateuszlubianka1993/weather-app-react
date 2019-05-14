c201f053bfbf0443199f90ea34154855b902361b
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-05-14 14:24:14 +0200

    Updates

---
+++ precache-manifest.90638f38f5e4cc3840977522dff9a133.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e8ba9f3cb5e3ae15d57961831c409a0",
+    "url": "/weather-app-react/static/media/abstract-art-background-1040473.6e8ba9f3.jpg"
+  },
+  {
+    "revision": "b246a4fc453f9ee8e4e8",
+    "url": "/weather-app-react/static/js/runtime~main.a31b442a.js"
+  },
+  {
+    "revision": "0a19f838a1df266f57ed",
+    "url": "/weather-app-react/static/js/main.cc5297d3.chunk.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/weather-app-react/static/js/2.88ea1fd6.chunk.js"
+  },
+  {
+    "revision": "0a19f838a1df266f57ed",
+    "url": "/weather-app-react/static/css/main.a82bcc89.chunk.css"
+  },
+  {
+    "revision": "073f978d6847c19528d349fa2b54ae8f",
+    "url": "/weather-app-react/index.html"
+  }
+];
